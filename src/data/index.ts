87ab5bf16681fa79e1a6d3fa66c8e03748bc0a5e
tamaralasbritto <tamaralasbritto@gmail.com>
2026-08-16 00:43:14 -0300

    feat: include remaining regulation blocks through article 209

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,8 +3,19 @@ import { adminDevices } from './regimento-admin';
 import { areaDevices3769 } from './regimento-areas-37-69';
 import { leisureDevices7090 } from './regimento-lazer-70-90';
 import { leisureDevices91125 } from './regimento-lazer-91-125';
+import { privateDevices126167 } from './regimento-privativas-126-167';
+import { finalDevices168209 } from './regimento-finais-168-209';
 
 export type { Device, ManifestationChoice } from './regimento';
 
-export const devices = [...baseDevices, ...adminDevices, ...areaDevices3769, ...leisureDevices7090, ...leisureDevices91125].sort((a,b)=>a.article-b.article || a.id.localeCompare(b.id));
+export const devices = [
+  ...baseDevices,
+  ...adminDevices,
+  ...areaDevices3769,
+  ...leisureDevices7090,
+  ...leisureDevices91125,
+  ...privateDevices126167,
+  ...finalDevices168209,
+].sort((a,b)=>a.article-b.article || a.id.localeCompare(b.id));
+
 export const themes = [...new Set(devices.map(device=>device.theme))];
