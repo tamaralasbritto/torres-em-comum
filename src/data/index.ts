61a0c8e66e9b823609bb8319db8643b9c024f802
tamaralasbritto <tamaralasbritto@gmail.com>
2026-08-16 00:39:27 -0300

    feat: include common-area regulation block

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,8 @@
 import { devices as baseDevices } from './regimento';
 import { adminDevices } from './regimento-admin';
+import { areaDevices3769 } from './regimento-areas-37-69';
 
 export type { Device, ManifestationChoice } from './regimento';
 
-export const devices = [...baseDevices, ...adminDevices].sort((a,b)=>a.article-b.article || a.id.localeCompare(b.id));
+export const devices = [...baseDevices, ...adminDevices, ...areaDevices3769].sort((a,b)=>a.article-b.article || a.id.localeCompare(b.id));
 export const themes = [...new Set(devices.map(device=>device.theme))];
