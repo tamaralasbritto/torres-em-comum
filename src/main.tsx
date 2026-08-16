5e1ceb465edfa50b0f00d0ec2833f60c3a293dbe
tamaralasbritto <tamaralasbritto@gmail.com>
2026-08-16 00:38:05 -0300

    feat: use combined regulation dataset

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import { devices, type ManifestationChoice } from './data/regimento';
+import { devices, type ManifestationChoice } from './data';
 import { backendStatus, loadAggregate, loadParticipant, loadResponses, saveParticipant, saveResponses, type Participant, type Responses } from './lib/data';
 
 const OFFICE_EMAIL='tercio@tercioguilhermeadv.com';
