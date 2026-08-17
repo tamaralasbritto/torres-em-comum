e0fb554446076007cfda59720cef47309133cb1f
tamaralasbritto <tamaralasbritto@gmail.com>
2026-08-16 21:28:52 -0300

    feat: route glossary separately from voting flow

--- src/entry.tsx
+++ src/entry.tsx
@@ -6,9 +6,12 @@ import { AdminPanel } from './admin';
 
 const hash=window.location.hash;
 const isAdminRoute=hash==='#admin'||hash.includes('access_token=')||hash.includes('error_description=');
+const isGlossaryRoute=hash==='#glossario';
 
 if(isAdminRoute){
   ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AdminPanel/></React.StrictMode>);
+}else if(isGlossaryRoute){
+  import('./glossary').then(({Glossary})=>ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><Glossary/></React.StrictMode>));
 }else{
   import('./topicMain');
 }
